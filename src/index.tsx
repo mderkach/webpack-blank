6c7cbd61739780ef1025d101ce6f8e9d0a0e3e5c
Деркач Михаил Игоревич <mderkach@phoenixit.ru>
2022-03-04 15:51:45 +0300

    fix: remove unused [test-0]

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.scss';
-import {App} from './components';
 
-render(<App />, document.getElementById('root'));
-reportWebVitals(console.log);
+render(<p>hello</p>, document.getElementById('root'));
+reportWebVitals(process.env.NODE_ENV !== 'production' ? console.log : undefined);
